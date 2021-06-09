8bd26c978723a8c462536011697d0899609e9f15
Alexandre Perdomo <aperdomo@free.fr>
2021-06-09 12:48:41 +0200

    add function to push to GCS + methods to check if coordinates in marseille + coordinates generator and counter

--- Hack4Nature/google_maps_static_requests/lib.py
+++ Hack4Nature/google_maps_static_requests/lib.py
@@ -2,10 +2,15 @@ import googlemaps
 from datetime import datetime
 from dotenv import load_dotenv
 import os
+import json
+import numpy as np
+import itertools
 
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
+from shapely.geometry.multipolygon import MultiPolygon
 
+from Hack4Nature.google_cloud_storage.lib import upload_to_gcp
 load_dotenv()
 
 def generate_maps_client():
@@ -20,40 +25,78 @@ def request_image(params):
 							 maptype=params['maptype'])
 	return image
 
-def export_image(filename, params):
+def save_image(filename, params):
 	image = request_image(params)
 	with open(f"raw_data/{filename}", "wb") as fp:
-	    for chunk in image:
-	        fp.write(chunk)
+		for chunk in image:
+			fp.write(chunk)
 	return True
 
-point = Point(0.5, 0.5)
-polygon = Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])
-print(polygon.contains(point))
+def delete_image(filename):
+	os.remove(filename)
+
+def coordinates_in_city(lat,lon):
+	point = Point(lon,lat)
+	with open('Hack4Nature/data/limit_marseille.json') as json_file:
+		data = json.load(json_file)
+		data_polygons = data['features'][0]["geometry"]['coordinates']
+		polygons = []
+		for data_polygon in data_polygons:
+			polygon = Polygon(data_polygon[0])
+			polygons.append(polygon)
+	multipolygon = MultiPolygon(polygons)
+	return multipolygon.contains(point)
+
+# WARNING !!!! Les coordonnées sont à donner en (lat,lon) mais les limites sont en (lon,lat)
+# coordinates_in_city(43.3032768,5.4800)
 
 
-params = {'center': '43.3032768,5.4800',
-		  'zoom':20,
-		  'size': (2560,2560),
-		  'scale':2,
-		  'maptype': 'satellite'}
-export_image("lib_export_test.png", params)
+def generate_square_coordinates(start_lat, start_lon, end_lat, end_lon, pas_lat, pas_lon):
+	lat_pts = np.arange(start_lat, end_lat, pas_lat).tolist()
+	lon_pts = np.arange(start_lon, end_lon, pas_lon).tolist()
+	all_coordinates = np.array(list(itertools.product(lat_pts, lon_pts)))
+	print(len(all_coordinates))
+	# in_marseille_coordinates = list(
+	# 	filter(lambda coordinates: coordinates_in_city(coordinates[0],coordinates[1]), all_coordinates))
+	in_marseille_coordinates = [x for x in all_coordinates if coordinates_in_city(x[0],x[1])]
+	print(len(in_marseille_coordinates))
+	return in_marseille_coordinates
 
-params = {'center': '43.3032768,5.4805',
-		  'zoom':20,
-		  'size': (2560,2560),
-		  'scale':2,
-		  'maptype': 'satellite'}
-export_image("lib_export_test_2.png", params)
+# En bas => 43.197224 (arrondir à 43.2)
+# En haut => 43.391017 (arrondir à 43.4)
+# A gauche => 5.278058 (arrondir à 5.28)
+# A droite => 5.532462 (arrondir à 5.54)
+generate_square_coordinates(43.20, 5.28, 43.40, 5.54, 0.0006, 0.0008)
 
-# Sur la longitude, un pas de 0.001 est trop grand.
-# Sur la longitude, un pas de 0.0005 est trop petit ? 
+def generate_marseille_images(start_lat, start_lon, end_lat, end_lon, pas_lat, pas_lon):
+	positions = generate_square_coordinates(start_lat, start_lon, end_lat, end_lon, pas_lat, pas_lon)
+	for position in positions:
+		lat = position[0]
+		lon = position[1]
+		params = {'center': f"{lat},{lon}",
+				  'zoom':20,
+				  'size': (2560,2560),
+				  'scale':2,
+				  'maptype': 'satellite'}
+		save_image(f"labels_{lat_pos}_{lon_pos}.png", params)
+		upload_to_gcp(f"labels_{lat_pos}_{lon_pos}.png")
+		delete_image(f"raw_data/labels_{lat_pos}_{lon_pos}.png")
 
-# En haut => 43.391017 (arrondir à 43.395)
-# A gauche => 5.278058 (arrondir à 5.280)
-# A droite => 5.532462 (arrondir à 5.535)
-# En bas => 43.197224 (arrondir à 43.195)
-
-# nombre de photos hauteur
-p_haut = (43.395 - 43.195) / 0.0005
-p_bas = (5.535 - 5.28) / 0.0005
+# Code utilisé pour générer les images de training.
+#Départ de Castellane : 43.2863, 5.3829
+# lat = 43.2863
+# lon = 5.3829
+# pas_lat = 0.0006
+# pas_lon = 0.0008
+# for i in range(10):
+# 	for j in range(10):
+# 		lat_pos = lat + pas_lat*i
+# 		lon_pos = lon + pas_lon*j
+# 		params = {'center': f"{lat_pos},{lon_pos}",
+# 				  'zoom':20,
+# 				  'size': (2560,2560),
+# 				  'scale':2,
+# 				  'maptype': 'satellite'}
+# 		save_image(f"labels_{lat_pos}_{lon_pos}.png", params)
+# 		upload_to_gcp(f"labels_{lat_pos}_{lon_pos}.png")
+# 		delete_image(f"raw_data/labels_{lat_pos}_{lon_pos}.png")
