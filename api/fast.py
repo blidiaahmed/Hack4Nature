1fc7f4483e506f0d1c196d273ac8c4c5d03f9da4
ahmed blidia <ahmed.blidia.math@gmail.com>
2021-06-15 14:46:30 +0200

    ee

--- api/fast.py
+++ api/fast.py
@@ -90,5 +90,5 @@ def index(image):
     model = load("model.joblib")
     df = model.predict_image(image=image,return_plot=False)
     annotated_image=model.predict_image(image=image,return_plot =True)
-    datas = calculate_tree_positions(df, float(latitude),float(longitude), service)
+    
     return {"data":datas,"image":annotated_image.tolist()}
